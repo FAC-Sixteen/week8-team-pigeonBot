845c6a77059f435a8f9a3901f38fa3353f55e6a0
Bobby <bobbysebolao@gmail.com>
2019-04-25 11:55:34 +0100

    Create controller home route

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,13 @@
 const queries = require('../model/queries/index.js'); // getData + postEditor queries
+
+const getData = require('../model/queries/getData');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('home', {
+    editors: getData
+  })
+});
+
+module.exports = router;
